4b5c5b4d660a825e18db8321228e179ffbe79433
Riderd19 <rider19010@hotmail.com>
2026-08-16 04:35:02 -0500

    Updated Tracking.

--- src/App.jsx
+++ src/App.jsx
@@ -100,7 +100,7 @@ export default function App() {
     return (
       <div className="min-h-screen w-full bg-white flex flex-col">
         <SiteHeader onVolver={handleVolver} />
-        <div className="w-full max-w-[2000px] min-h-screen mx-auto flex-1 flex flex-col gap-6 px-4 sm:px-8 py-10">
+        <div className="w-full max-w-[2000px] min-h-screen mx-auto flex-1 flex flex-col gap-6 px-4 pt-6 pb-10 sm:px-8">
           <OrderResult pedido={pedido} />
         </div>
         <SiteFooter />
